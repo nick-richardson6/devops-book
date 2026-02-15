c0968a0671ea4faa6cd77a80478a9f50424c7606
Nick Richardson <nrichardson@mail.weber.edu>
2026-02-15 16:20:17 -0700

    Fix github_repo for OIDC

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -18,7 +18,7 @@ module "iam_roles" {
   enable_iam_role_for_testing = true                            
 
   # TODO: fill in your own repo name here!
-  github_repo      = "CodySquadroni/devops-book" 
+  github_repo      = "nick-richardson6/devops-book" 
   lambda_base_name = "lambda-sample"                            
 
   enable_iam_role_for_plan  = true                                
